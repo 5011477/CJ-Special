90fbe6894771a05ff5c3a403f346967f8df107e7
5011477 <5011477@qq.com>
2014-12-07 00:33:51 +0800

    更新 ADC合集  易  泽拉斯

--- 打野/【無為汉化】剑圣/Master Yi by Prunes/Properties/AssemblyInfo.cs
+++ 打野/【無為汉化】剑圣/Master Yi by Prunes/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Xerath")]
+[assembly: AssemblyTitle("Master Yi by Prunes")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Xerath")]
+[assembly: AssemblyProduct("Master Yi by Prunes")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1ddffac5-57c5-4467-baa9-55843ec18f73")]
+[assembly: Guid("7915586c-19ae-40cb-a0a7-0adabd78c660")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
